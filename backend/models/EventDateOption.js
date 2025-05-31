2313a5ebc968dab3976a2a5bdd068f87087ea3c0
jozef134 <ivancic.jozef@gmail.com>
2025-06-01 01:14:35 +0200

    Dodan EventDateOption.js (za izbiro datumov)

---
+++ backend/models/EventDateOption.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+const Event = require('./Event'); // Import Event model
+
+const EventDateOption = sequelize.define('EventDateOption', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    eventId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        references: {
+            model: 'Events',
+            key: 'id'
+        },
+        onDelete: 'CASCADE'
+    },
+    dateOption: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    isFinal: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    }
+});
+
+// Določi povezavo
+EventDateOption.belongsTo(Event, { foreignKey: 'eventId' });
+Event.hasMany(EventDateOption, { foreignKey: 'eventId' });
+
+module.exports = EventDateOption;
