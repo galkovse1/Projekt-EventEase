c5ef8f515b0f3ced048bc5a4cbe40d639cf42fe9
jozef134 <ivancic.jozef@gmail.com>
2025-06-04 14:29:03 +0200

    Dodajanje allModels.js

---
+++ backend/models/allModels.js
@@ -0,0 +1,15 @@
+const User = require('./User');
+const Event = require('./Event');
+const EventSignup = require('./EventSignup');
+const EventVisibility = require('./EventVisibility');
+const EventDateOption = require('./EventDateOption');
+const DateVote = require('./DateVote');
+
+module.exports = {
+    User,
+    Event,
+    EventSignup,
+    EventVisibility,
+    EventDateOption,
+    DateVote
+};
